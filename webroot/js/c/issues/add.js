1151b39439f3e44f9f7bde2239600802999789f5
kiang <kiange@gmail.com>
2019-07-25 13:55:38 +0800

    add js

---
+++ webroot/js/c/issues/add.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#IssueReported').datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+    $('#IssueConfirmed').datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+    $('#IssueDateOnset').datepicker({
+        dateFormat: 'yy-mm-dd'
+    });
+});
