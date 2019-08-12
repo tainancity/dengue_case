b2e19d1050370fb14a459afee0e69ab9e5530013
kiang <kiange@gmail.com>
2019-08-12 14:43:31 +0800

    to import center source data

--- Console/Command/CenterSourceShell.php
+++ Console/Command/CenterSourceShell.php
@@ -9,11 +9,40 @@ class CenterSourceShell extends AppShell {
     }
 
     public function import() {
+        $areas = $this->CenterSource->Area->find('all', array(
+            'fields' => array('Area.id', 'Area.parent_id', 'name'),
+            'conditions' => array('Area.parent_id IS NOT NULL'),
+            'contain' => array('Parent' => array(
+                    'fields' => array('id', 'name'),
+                )),
+        ));
+        $areaList = array();
+        foreach ($areas AS $area) {
+            $areaList["{$area['Parent']['name']}{$area['Area']['name']}"] = $area['Area']['id'];
+        }
         $pdoConfig = Configure::read('pdo');
         $dbh = new PDO($pdoConfig['odbc'], $pdoConfig['id'], $pdoConfig['password']);
-        $result = $dbh->query('SELECT TOP 10 * FROM Mosquito_Density1 WHERE DATE = CAST(\'2019-08-11\' AS DATE) ORDER BY DATE DESC');
-        foreach ($result as $row) {
-            print_r($row);
+        //$today = date('Y-m-d');
+        //$result = $dbh->query('SELECT * FROM Mosquito_Density1 WHERE DATE = CAST(\'' . $today . '\' AS DATE)');
+        $result = $dbh->query('SELECT * FROM Mosquito_Density1 ORDER BY DATE DESC');
+        if ($result) {
+            foreach ($result as $row) {
+                $areaKey = $row['DISTRICT_NAME'] . $row['VILLAGE_NAME'];
+                if (!isset($areaList[$areaKey])) {
+                    echo "{$areaKey}\n";
+                } else {
+                    $this->CenterSource->create();
+                    $this->CenterSource->save(array('CenterSource' => array(
+                            'area_id' => $areaList[$areaKey],
+                            'investigate' => $row['SURVEY_HOUSEHOLD'],
+                            'positive_done' => $row['POSITIVE_HOUSEHOLD'],
+                            'i_water' => $row['SURVEY_CONTAINER_IN'],
+                            'o_water' => $row['SURVEY_CONTAINER_OUT'],
+                            'i_positive' => $row['POSITIVE_CONTAINER_IN'],
+                            'o_positive' => $row['POSITIVE_CONTAINER_OUT'],
+                    )));
+                }
+            }
         }
     }
 
